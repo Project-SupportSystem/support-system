670993b0110d9580d9fc3c0e34d814423804ed95
TewaranChaiyakit <rungoodnam@gmail.com>
2024-10-03 02:56:20 +0700

    hot fix

--- resources/views/table_report.blade.php
+++ resources/views/table_report.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>ระบบบันทึกผลการศึกษา</title>
+    <title>ระบบบันทึกผลการศึกษานักศึกษา</title>
 
     <style>
         body {
